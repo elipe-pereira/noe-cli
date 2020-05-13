54f4196fad50a423c1e443e7ced1b0be7927cc13
elip <elip@elipereira.com.br>
2020-05-13 00:21:10 -0300

    Adicionado backup local da pasta configurada.

---
+++ usr/share/noe/noe.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+import configparser
+from datetime import date
+from pathlib import  PurePath
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read("/etc/noe/noe.conf")
+
+    for section in config.sections():
+        folder_dest = config.get(section, 'folder_dest')
+        folder_backup = config.get(section, 'folder_backup')
+        type_backup = config.get(section, 'type_backup')
+        file = PurePath(folder_backup).name
+        filename = "{0}-{1}-{2}".format(file, section, date.today())
+
+        if type_backup == 'local':
+            os.system("tar -zcvf {0}/{1}.tar.gz {2}".format(folder_dest, filename, folder_backup))
+
+
+if __name__ == '__main__':
+    main()

53c69b0b61c4addb1ae28fa01baaf481e35a77b0
elip <elip@elipereira.com.br>
2020-05-13 00:51:15 -0300

    Adicionado verificação se a pasta de destino existe.

--- usr/share/noe/noe.py
+++ usr/share/noe/noe.py
@@ -3,7 +3,7 @@
 import os
 import configparser
 from datetime import date
-from pathlib import  PurePath
+from pathlib import PurePath
 
 
 def main():
@@ -16,6 +16,9 @@ def main():
         type_backup = config.get(section, 'type_backup')
         file = PurePath(folder_backup).name
         filename = "{0}-{1}-{2}".format(file, section, date.today())
+
+        if not os.path.isdir(folder_dest):
+            os.mkdir(folder_dest)
 
         if type_backup == 'local':
             os.system("tar -zcvf {0}/{1}.tar.gz {2}".format(folder_dest, filename, folder_backup))

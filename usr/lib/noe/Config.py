4b886f279bac276fd1d28fa4ba793606d162b08b
elip <elip@elipereira.com.br>
2020-05-18 22:35:29 -0300

    Aplicado ajustes no código deixando ele orientado a objetos.

--- usr/lib/noe/Config.py
+++ usr/lib/noe/Config.py
@@ -1,9 +1,101 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import configparser
+from datetime import date
 
 
 class Config(object):
     def __init__(self):
-        self.
+        self._config_file = "/etc/noe/noe.conf"
+        self._config = configparser.ConfigParser()
+        self._config.read(self._config_file)
+        self._sections_config = self._config.sections()
+        self._type_backup = "local"
+        self._folder_backup = "/"
+        self._folder_dest = "/mnt/backup"
+        self._remote_share = ""
+        self._host = "localhost"
+        self._user = "user"
+        self._password = "NOPASS"
+        self._bucket_name = "bucket"
+        self._access_key = "noaccesskey"
+        self._secret_access_key = "nosecret"
+        self._filename = "{0}-00:00:00".format("default")
+        self._exclude_list_file = "exclude_list.txt"
+
+    def getSectionsConfig(self):
+        return self._sections_config
+
+    def setTypeConfig(self, section, type_backup):
+        self._type_backup = self._config.get(section, type_backup)
+
+    def getTypeConfig(self):
+        return self._type_backup
+
+    def setFolderConfig(self, section, folder_backup):
+        self._folder_backup = self._config.get(section, folder_backup)
+
+    def getFolderConfig(self):
+        return self._folder_backup
+
+    def setFolderDestConfig(self, section, folder_dest):
+        self._folder_dest = self._config.get(section, folder_dest)
+
+    def getFolderDestConfig(self):
+        return self._folder_dest
+
+    def setRemoteShareConfig(self, section, remote_share):
+        self._remote_share = self._config.get(section, remote_share)
+
+    def getRemoteShareConfig(self):
+        return self._remote_share
+
+    def setHostConfig(self, host):
+        self._host = host
+
+    def getHostConfig(self):
+        return self._host
+
+    def setUserConfig(self, section, user):
+        self._user = self._config.get(section, user)
+
+    def getUserConfig(self):
+        return self._user
+
+    def setPasswordConfig(self, section, password):
+        self._password = self._config.get(section, password)
+
+    def getPasswordConfig(self):
+        return self._password
+
+    def setBucketNameConfig(self, section, bucket_name):
+        self._bucket_name = self._config.get(section, bucket_name)
+
+    def getBucketNameConfig(self):
+        return self._bucket_name
+
+    def setAccessKeyConfig(self, section, access_key):
+        self._access_key = self._config.get(section, access_key)
+
+    def getAccessKeyConfig(self):
+        return self._access_key
+
+    def setSecretAccessKey(self, section, secret_access_key):
+        self._secret_access_key = self._config.get(section, secret_access_key)
+
+    def getSecretAccessKey(self):
+        return self._secret_access_key
+
+    def setFileNameConfig(self, section, date):
+        self._filename = "{0}-{1}".format(section, date)
+
+    def getFileNameConfig(self):
+        return self._filename
+
+    def setExcludeListFile(self, section, exclude_list_file):
+        self._exclude_list_file = self._config.get(section, exclude_list_file)
+
+    def getExcludeListFile(self):
+        return self._exclude_list_file
 
 
--- usr/share/noe/noe.py
+++ usr/share/noe/noe.py
@@ -1,59 +1,66 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
-import configparser
+import sys
 from datetime import date
-import usr.lib.noe.noe
+sys.path.inser("0", "/usr/lib/noe")
+from .Config import Config
+from .Backup import Backup
+from .Mount import Mount
 
 
 def main():
-    config = configparser.ConfigParser()
-    config.read("/etc/noe/noe.conf")
+    #config = configparser.ConfigParser()
+    #config.read("/etc/noe/noe.conf")
+    config = Config()
+    backup = Backup()
+    mount = Mount()
+    sections = config.getSectionsConfig()
 
-    for section in config.sections():
-        type_backup = config.get(section, 'type_backup')
-        folder_backup = config.get(section, 'folder_backup')
-        folder_dest = config.get(section, 'folder_dest')
-        remote_share = config.get(section, 'remote_share')
-        host = config.get(section, 'host')
-        user = config.get(section, 'user')
-        password = config.get(section, 'password')
-        bucket_name = config.get(section, 'bucket_name')
-        access_key = config.get(section, 'access_key')
-        secret_access_key = config.get(section, 'secret_access_key')
+    for section in sections:
+        config.setTypeConfig(section, 'type_backup')
+        config.setFolderConfig(section, 'folder_backup')
+        config.setFolderDestConfig(section, 'folder_dest')
+        config.setRemoteShareConfig(section, 'remote_share')
+        config.setHostConfig(section, 'host')
+        config.setUserConfig(section, 'user')
+        config.setPasswordConfig(section, 'password')
+        config.setBucketNameConfig(section, 'bucket_name')
+        config.setAccessKeyConfig(section, 'access_key')
+        config.setSecretAccessKey(section, 'secret_access_key')
+        config.setFileNameConfig(section, date.today())
+        config.setExcludeListFile(section, 'exclude_list_file')
 
-        if folder_backup == "/":
-            file = "full"
-        else:
-            file = "partial"
-
-        filename = "{0}-{1}-{2}".format(file, section, date.today())
-        exclude_list_file = config.get(section, 'exclude_list_file')
+        type_backup = config.getTypeConfig()
+        folder_backup = config.getFolderConfig()
+        folder_dest = config.getFolderDestConfig()
+        remote_share = config.getRemoteShareConfig()
+        host = config.getHostConfig()
+        user = config.getUserConfig()
+        password = config.getPasswordConfig()
+        bucket_name = config.getBucketNameConfig()
+        access_key = config.getAccessKeyConfig()
+        secret_access_key = config.getSecretAccessKey()
+        filename = config.getFileNameConfig()
+        exclude_list_file = config.getExcludeListFile()
 
         if not os.path.isdir(folder_dest):
             os.mkdir(folder_dest)
 
         if type_backup == "local":
-            os.system("tar --exclude-from={0} --exclude={1} -zcvf {2}/{3}.tar.gz {4}"
-                      .format(exclude_list_file, folder_dest, folder_dest, filename, folder_backup))
+            backup.run(exclude_list_file, folder_dest, filename, folder_backup)
 
         elif type_backup == "samba":
-            os.system("mount //{0}/{1} {2} -o username={3},password={4} || exit 1"
-                            .format(host, remote_share, folder_dest, user, password))
-            os.system("tar --exclude-from={0} --exclude={1} -zcvf {2}/{3}.tar.gz {4}"
-                          .format(exclude_list_file, folder_dest, folder_dest, filename, folder_backup))
-            os.system("umount {0}".format(folder_dest))
+            mount.mountSamba(host, remote_share, folder_dest, user, password)
+            backup.run(exclude_list_file, folder_dest, filename, folder_backup)
+            mount.umount(folder_dest)
+
 
         elif type_backup == "bucket":
             tmp_file = "/tmp/.passwd-s3fs"
-
-            os.system("echo {0}:{1} > {2}".format(access_key, secret_access_key, tmp_file))
-            os.chmod(tmp_file, 0O600)
-            os.system("s3fs {0} {1} -o passwd_file={2} -o use_path_request_style"
-                      .format(bucket_name, folder_dest, tmp_file))
-            os.system("tar --exclude-from={0} --exclude={1} -zcvf {2}/{3}.tar.gz {4}"
-                      .format(exclude_list_file, folder_dest, folder_dest, filename, folder_backup))
-            os.system("umount {0}".format(folder_dest))
+            mount.mountBucket(access_key, secret_access_key, tmp_file, bucket_name, folder_dest)
+            backup.run(exclude_list_file, folder_dest, filename, folder_backup)
+            mount.umount(folder_dest)
 
         else:
             print("Tipo de backup não válido")

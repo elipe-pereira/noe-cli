efef78e470a3b339f71aaf2f2fcb7397628753d9
Eli Florêncio Pereira <eli@mail.hanokh.com.br>
2020-09-03 07:06:52 -0300

    Adicinado ajustes no envio de e-mail

--- Mail.py
+++ Mail.py
@@ -7,6 +7,7 @@ import os
 class Mail(object):
 	@staticmethod
 	def send(subject, mail_address):
+		os.system("mkdir /tmp/Mail")
 		file = open("{0}/{1}".format("/tmp", ".muttrc_tmp"), "+w")
 		file.write("set use_from=yes\n")
 		file.write("set mbox=+Inbox\n")
@@ -19,4 +20,4 @@ class Mail(object):
 		file.write("set my_pass='dhDe(eEP96#1JO'\n")
 		file.write("set smtp_url=smtp://$my_user:$my_pass@mail.hanokh.com.br:587\n")
 		file.close()
-		os.system("cat /var/log/noe.log|mutt -F /tmp/.muttrc_tmp -s '{0}' {1}".format(subject, mail_address))
+		os.system("cat /var/log/noe.log|mutt -f /tmp/Mail -F /tmp/.muttrc_tmp -s '{0}' {1}".format(subject, mail_address))

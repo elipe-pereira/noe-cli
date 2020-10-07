b911ae771ef19b7c494369eb8f9a9b408294698a
Eli Pereira <elipe.pereira@gmail.com>
2020-10-07 14:56:29 -0300

    Ajustado local-sync-onedrive para fazer a sincronização somente da pasta onde está os backup com o onedrive, desabilitando somente o envio via upload que colocado em outro parametro de configuração

--- noe.py
+++ noe.py
@@ -15,7 +15,7 @@ def main():
     main_file_exec_path = os.path.realpath(sys.argv[0])
     working_dir = os.path.dirname(main_file_exec_path)
     config_file = working_dir + "/config/noe/noe.conf"
-    
+
     config = Config(config_file)
     backup = Backup()
     mount = Mount()
@@ -43,7 +43,7 @@ def main():
     if enable_stop_services == "yes":
         log.log("Parando serviços")
         for command in command_stop:
-            command_exec.stop_service(command)
+/            command_exec.stop_service(command)
 
     for section in sections:
         if section == 'DEFAULT':
@@ -97,7 +97,14 @@ def main():
             backup.run(exclude_list_file, folder_dest, filename, folder_backup)
             log.log("Fim do backup " + section)
 
-            onedrive_sync_flag = 1
+            local_sync_onedrive_flag = 1
+
+        elif type_backup == "send-file-onedrive":
+            log.log("Backup do tipo send-file-onedrive")
+            backup.run(exclude_list_file, folder_dest, filename, folder_backup)
+            log.log("Fim do backup " + section)
+
+            send_file_onedrive_flag = 1
 
         elif type_backup == "samba":
             log.log("Executando backup do tipo samba")
@@ -140,10 +147,17 @@ def main():
         for command in command_start:
             command_exec.start_service(command)
 
-    if onedrive_sync_flag == 1:
-        log.log("Sincronizando com a nuvem")
+    if local_sync_onedrive_flag == 1:
+        log.log("Sincronizando pasta de backup com a nuvem")
+        folder_sync_onedrive = os.path.basename(folder_dest)
+        os.system("onedrive --synchronize --single-directory {0}".format(folder_sync_onedrive))
+        log.log("Envio concluído")
+
+    elif send_file_onedrive_flag == 1:
+        log.log("Enviando backup via upload para o onedrive")
         os.system("onedrive --synchronize --upload-only --no-remote-delete")
         log.log("Envio concluído")
+
 
     config.set_mail_address('DEFAULT', 'mail_address')
     mail_address = config.get_mail_address()

00bf31cfdbd0d0cd29cf594a0bd90684646cc17d
Eli <eli@hanokh.com.br>
2023-10-10 00:16:39 -0300

    Ajustes

--- log.py
+++ log.py
@@ -3,22 +3,23 @@
 
 import logging
 
-class Log(object):
-	def __init__(self):
-		self.__name = "Backup"
-		self.__log_file = "/var/log/noe.log"
-		self.__log = logging.getLogger(self.__name)
-		self.__log.setLevel(logging.DEBUG)
-		self.__fh = logging.FileHandler(self.__log_file)
-		self.__fh.setLevel(logging.DEBUG)
-		self.__format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-		self.__fh.setFormatter(self.__format)
-		self.__log.addHandler(self.__fh)
 
+class Log:
+    def __init__(self):
+        self.name = "Backup"
+        self.log_file = "/var/log/noe.log"
+        self.log = logging.getLogger(self.name)
+        self.log.setLevel(logging.DEBUG)
+        self.fh = logging.FileHandler(self.log_file)
+        self.fh.setLevel(logging.DEBUG)
+        self.format = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+            )
+        self.fh.setFormatter(self.format)
+        self.log.addHandler(self.fh)
 
-	def get_log_file(self):
-		return self.__log_file
-		
+    def get_log_file(self):
+        return self.log_file
 
-	def log(self, message):
-		self.__log.info(message)
+    def log(self, message):
+        self.log.info(message)
--- services.py
+++ services.py
@@ -3,11 +3,10 @@
 
 import os
 
+
 class Services(object):
+    def stop_service(self, command_stop):
+        os.system(command_stop)
 
-	def stop_service(self, command_stop):
-		os.system(command_stop)
-
-
-	def start_service(self, command_start):
-		os.system(command_start)
+    def start_service(self, command_start):
+        os.system(command_start)
